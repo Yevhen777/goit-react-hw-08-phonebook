9ba4820a3f9b9400de26856b3d59f488ad0b7629
Yevhen <92806371+Yevhen777@users.noreply.github.com>
2022-07-26 12:36:19 +0300

    hw-4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -33,7 +33,6 @@ export class App extends Component {
   componentDidUpdate(prevProps, prevState) {
     if (this.state.contacts !== prevState.contacts) {
       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-      console.log('Обновился локал');
     }
   }
 
